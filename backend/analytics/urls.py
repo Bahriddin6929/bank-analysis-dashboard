a2ef6e4bebde7ee651836d53707673dff45ead2d
Bahriddin6929 <bahriddin6929@gmail.com>
2026-05-15 02:42:08 +0500

    Update urls.py

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -5,20 +5,21 @@ from django.http import HttpResponse
 
 def oson_migrate(request):
     try:
+        call_command('makemigrations', 'analytics')
         call_command('migrate')
         return HttpResponse("✅ BAZA YARATILDI! Saytga qaytib bemalol saqlashingiz mumkin.")
     except Exception as e:
         return HttpResponse(f"Xato: {e}")
 
 urlpatterns = [
-    # Baza yaratish uchun maxsus manzil:
     path('baza-yaratish/', oson_migrate),
-    
-    # Sizning eski (ishlaydigan) manzillaringiz:
     path('summary', views.get_summary, name='summary'),
     path('top-customers', views.get_top_customers, name='top_customers'),
     path('transactions', views.get_transactions, name='transactions'),
     path('customers', views.get_customers, name='customers'),
     path('add-customer', views.add_customer, name='add_customer'),
     path('add-transaction', views.add_transaction, name='add_transaction'),
+    
+    # YANGI QO'SHILDI: O'chirish uchun manzil
+    path('delete-customer/<int:id>', views.delete_customer, name='delete_customer'),
 ]
