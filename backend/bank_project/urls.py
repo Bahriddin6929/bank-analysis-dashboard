dd965c0be756f4e6478241e3311fe5b72d13987a
Bahriddin6929 <bahriddin6929@gmail.com>
2026-05-15 02:02:27 +0500

    Update urls.py

--- backend/bank_project/urls.py
+++ backend/bank_project/urls.py
@@ -1,22 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.core.management import call_command
-from django.http import HttpResponse
-
-def oson_migrate(request):
-    try:
-        call_command('makemigrations', 'analytics') # <-- Shuyerga so'z qo'shildi
-        call_command('migrate')
-        return HttpResponse("✅ BAZA YARATILDI! Saytga qaytib bemalol saqlashingiz mumkin.")
-    except Exception as e:
-        return HttpResponse(f"Xato: {e}")
 
 urlpatterns = [
-    # Baza yaratish uchun maxsus manzil:
-    path('baza-yaratish/', oson_migrate),
-    
-    # Sizning eski manzillaringiz:
     path('admin/', admin.site.urls),
     path('api/', include('analytics.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
