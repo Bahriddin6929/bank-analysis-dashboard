eadf6ec01389e6820809d1258374f982047f3fc7
Bahriddin6929 <bahriddin6929@gmail.com>
2026-05-15 00:52:19 +0500

    Update wsgi.py

--- backend/bank_project/wsgi.py
+++ backend/bank_project/wsgi.py
@@ -1,4 +1,12 @@
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_project.settings')
 application = get_wsgi_application()
+
+# Biz qo'shgan kod:
+from django.core.management import call_command
+try:
+    call_command('migrate')
+except Exception as e:
+    pass
