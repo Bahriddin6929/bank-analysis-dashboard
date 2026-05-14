d3335451d85f8efe50c4563754fd6cbe250f1f38
Bahriddin6929 <bahriddin6929@gmail.com>
2026-05-15 02:40:11 +0500

    Update urls.py

--- backend/bank_project/urls.py
+++ backend/bank_project/urls.py
@@ -1,25 +1,9 @@
-from django.urls import path
-from . import views
-from django.core.management import call_command
-from django.http import HttpResponse
-
-def oson_migrate(request):
-    try:
-        call_command('makemigrations', 'analytics')
-        call_command('migrate')
-        return HttpResponse("✅ BAZA YARATILDI! Saytga qaytib bemalol saqlashingiz mumkin.")
-    except Exception as e:
-        return HttpResponse(f"Xato: {e}")
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('baza-yaratish/', oson_migrate),
-    path('summary', views.get_summary, name='summary'),
-    path('top-customers', views.get_top_customers, name='top_customers'),
-    path('transactions', views.get_transactions, name='transactions'),
-    path('customers', views.get_customers, name='customers'),
-    path('add-customer', views.add_customer, name='add_customer'),
-    path('add-transaction', views.add_transaction, name='add_transaction'),
-    
-    # YANGI QO'SHILDI: O'chirish uchun manzil
-    path('delete-customer/<int:id>', views.delete_customer, name='delete_customer'),
+    path('admin/', admin.site.urls),
+    path('api/', include('analytics.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
